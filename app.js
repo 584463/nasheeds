b1c351e8a7bf374220c0dfc780a5e253dd08e774
584463 <584463@eriesd.org>
2024-03-11 10:15:04 -0400

    packgese

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-mongoose.connect("mongodb://localhost:27017/hasana").then(() => {
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://mstr3sl:vip222111@nasheeds.8ovgh0e.mongodb.net/").then(() => {
             console.log('\x1b[32mConnected to MongoDB\x1b[37m');
         }).catch(err => {
             console.error('\x1b[31mConnection error\x1b[37m', err);
